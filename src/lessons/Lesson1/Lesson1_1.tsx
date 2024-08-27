1be9e785288337fa09bce278c69b6dfe17ff944e
TakuyaArai6I20VV <leetgrow@gmail.com>
2024-08-28 00:12:02 +0900

    useState

--- src/lessons/Lesson1/Lesson1_1.tsx
+++ src/lessons/Lesson1/Lesson1_1.tsx
@@ -1,9 +1,19 @@
+import { ChangeEvent, useState } from "react"
+
 const Lesson1_1 = () => {
+  const [age, setAge] = useState(0)
+  const [name, setName] = useState("taku")
+
+  const handleClick = () => {
+    setAge(age + 1)
+  }
+
   return (
     <div>
-      <input type="text" />
-      <button className="border p-2 rounded-md bg-red-100">Add Age</button>
-      <p></p>
+      <input className="border" type="text" value={name} onChange={(e: ChangeEvent<HTMLInputElement>) => setName(e.target.value)} />
+      <button className="border p-2 rounded-md bg-red-100" onClick={handleClick}>Add Age</button>
+      <p>You are {age}</p>
+      <p>Your name is {name}</p>
     </div>
   );
 };
